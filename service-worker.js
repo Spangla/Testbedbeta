c26bde3f91c619aa614c2da67b0ece174dbdd0d3
Spangla <zamwise@hotmail.co.uk>
2025-10-09 20:49:53 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,17 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("monster-mix-beta-v1").then((cache) => {
-      return cache.addAll([
-        "/monster-mix-beta/",
-        "/monster-mix-beta/index.html",
-        "/monster-mix-beta/style.css",
-        "/monster-mix-beta/script.js",
-        "/monster-mix-beta/icon-192-beta.png",
-        "/monster-mix-beta/icon-512-beta.png"
-      ]);
-    })
-  );
-});
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+const CACHE_NAME = 'monster-mix-beta-v2'; // bump this whenever assets change
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        '/monster-mix-beta/',
+        '/monster-mix-beta/index.html',
+        '/monster-mix-beta/style.css',
+        '/monster-mix-beta/script.js',
+        '/monster-mix-beta/icon-192-beta.png',
+        '/monster-mix-beta/icon-512-beta.png'
+      ])
+    )
   );
 });
