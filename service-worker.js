83daa6f1252073b941245c5e5e80453fa362a7f3
Spangla <zamwise@hotmail.co.uk>
2025-10-09 22:00:11 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,12 +5,12 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache =>
       cache.addAll([
-        '/Testbedbeta/',
-        '//Testbedbeta/index.html',
-        '/Testbedbeta/style.css',
-        '/Testbedbeta/script.js',
-        '/Testbedbeta/icon-192-beta.png',
-        '/Testbedbeta/icon-512-beta.png'
+       '/monster-mix-beta/',
+        '/monster-mix-beta/index.html',
+        '/monster-mix-beta/style.css',
+        '/monster-mix-beta/script.js',
+        '/monster-mix-beta/icon-192-beta.png',
+        '/monster-mix-beta/icon-512-beta.png'
       ])
     )
   );
