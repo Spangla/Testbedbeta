a4d587bebbb75086f7c1555acbdb2c18cd74f848
Spangla <zamwise@hotmail.co.uk>
2025-10-25 11:04:42 +0100

    Rename CACHE_NAME and update cached assets

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
 
-const CACHE_NAME = 'monster-mix-beta-v2'; // bump this whenever assets change
+const CACHE_NAME = 'Testbedbeta'; // bump this whenever assets change
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache =>
       cache.addAll([
-       '/Monster Mix/',
-        '/monster-mix-beta/index.html',
-        '/monster-mix-beta/style.css',
-        '/monster-mix-beta/script.js',
-        '/monster-mix-beta/icon-192-beta.png',
-        '/monster-mix-beta/icon-512-beta.png'
+       '/Testbedbeta/',
+        '/Testbedbeta/index.html',
+        '/Testbedbeta/style.css',
+        '/Testbedbeta/script.js',
+        '/Testbedbeta/icon-192-beta.png',
+        '/Testbedbeta/icon-512-beta.png'
       ])
     )
   );
