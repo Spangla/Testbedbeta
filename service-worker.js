a46080fb71e0c720eaf3dda0766eaa3088a1e331
Spangla <zamwise@hotmail.co.uk>
2026-05-20 21:33:58 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Testbedbeta-v2'; // bump version when updating files
+const CACHE_NAME = 'Testbedbeta-v3'; // bump version when updating files
 
 const ASSETS_TO_CACHE = [
   './',
